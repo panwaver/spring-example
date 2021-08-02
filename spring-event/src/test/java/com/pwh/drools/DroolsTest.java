8e2f07241d896ca4691b854b7986bd9542077c0b
panwaver <panwaver@163.com>
2021-08-02 16:26:54 +0800

    drools

--- spring-event/src/test/java/com/pwh/drools/DroolsTest.java
+++ spring-event/src/test/java/com/pwh/drools/DroolsTest.java
@@ -5,6 +5,8 @@ import org.kie.api.KieServices;
 import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
+import org.kie.internal.conf.MultithreadEvaluationOption;
 import org.kie.internal.utils.KieHelper;
 
 /**
@@ -69,5 +71,27 @@ class DroolsTest {
         ksession.fireAllRules();
 
         ksession.dispose();
+    }
+
+    @Test
+    public void executeTest() {
+        String myRule = "import com.pwh.drools.Person\n" +
+                "\n" +
+                "dialect  \"mvel\"\n" +
+                "\n" +
+                "rule \"age\"\n" +
+                "    when\n" +
+                "        $person : Person(age<16 || age>50)\n" +
+                "    then\n" +
+                "        System.out.println(\"这个人的年龄不符合要求！（基于动态加载）\");\n" +
+                "end\n";
+        KieHelper helper = new KieHelper();
+        helper.addContent(myRule, ResourceType.DRL);
+        StatelessKieSession kieSession = helper.build(MultithreadEvaluationOption.YES).newStatelessKieSession();
+        Person person = new Person();
+
+        person.setAge(12);
+        person.setName("Test");
+        kieSession.execute(person);
     }
 }
