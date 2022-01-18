b67378af4c43aad70e30fe0278991fba1008cba5
panwaver <panwaver@163.com>
2022-01-18 21:56:11 +0800

    两数相加

--- spring-event/src/main/java/com/pwh/leetcode/math/两数相加.java
+++ spring-event/src/main/java/com/pwh/leetcode/math/两数相加.java
@@ -16,11 +16,13 @@ public class 两数相加 {
             ListNode p = dummy;
             // 记录进位
             int carry = 0;
-            while (p1 !=null && p2 != null){
+            // 开始执行加法，两条链表走完且没有进位时才能结束循环
+            while (p1 != null || p2 != null || carry > 0) {
                 //先加上次的进位
                 int val = carry;
                 if (p1 != null){
                     val += p1.val;
+                    p1 = p1.next;
                 }
                 if (p2 != null){
                     val += p2.val;
@@ -33,10 +35,42 @@ public class 两数相加 {
                 p.next = new ListNode(val);
                 p = p.next;
             }
-
             return dummy.next;
         }
     }
+//class Solution {
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        // 在两条链表上的指针
+//        ListNode p1 = l1, p2 = l2;
+//        // 虚拟头结点（构建新链表时的常用技巧）
+//        ListNode dummy = new ListNode(-1);
+//        // 指针 p 负责构建新链表
+//        ListNode p = dummy;
+//        // 记录进位
+//        int carry = 0;
+//        // 开始执行加法，两条链表走完且没有进位时才能结束循环
+//        while (p1 != null || p2 != null || carry > 0) {
+//            // 先加上上次的进位
+//            int val = carry;
+//            if (p1 != null) {
+//                val += p1.val;
+//                p1 = p1.next;
+//            }
+//            if (p2 != null) {
+//                val += p2.val;
+//                p2 = p2.next;
+//            }
+//            // 处理进位情况
+//            carry = val / 10;
+//            val = val % 10;
+//            // 构建新节点
+//            p.next = new ListNode(val);
+//            p = p.next;
+//        }
+//        // 返回结果链表的头结点（去除虚拟头结点）
+//        return dummy.next;
+//    }
+//}
 
     public static void main(String[] args) {
         ListNode listNode = new ListNode(2);
@@ -45,22 +79,28 @@ public class 两数相加 {
         listNode.next = listNode1;
         listNode1.next = listNode2;
 
-        ListNode listNode5 = new ListNode(2);
-        ListNode listNode6 = new ListNode(4);
-        ListNode listNode4 = new ListNode(3);
+        ListNode listNode5 = new ListNode(5);
+        ListNode listNode6 = new ListNode(6);
+        ListNode listNode4 = new ListNode(4);
         listNode5.next = listNode6;
         listNode6.next = listNode4;
 //        System.out.println(listNode+"");
 
         ListNode resultNode = new 两数相加().new Solution().addTwoNumbers(listNode, listNode5);
 
-        do {
-            System.out.println(resultNode.val);
-            resultNode = resultNode.next;
-        }while (resultNode.next != null);
-//        while (resultNode.next != null){
+        for (ListNode l = resultNode; l!=null; l=l.next){
+            System.out.println(l.val);
+        }
+
+//        do {
+//            System.out.println(resultNode.val);
+//            resultNode = resultNode.next;
+//        }while (resultNode != null);
+
+//        while (resultNode != null){
 //            System.out.println(resultNode.val);
 //            resultNode = resultNode.next;
 //        }
+
     }
 }
