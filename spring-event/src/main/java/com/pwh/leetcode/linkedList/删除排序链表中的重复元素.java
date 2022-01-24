d1e3167f01f8789bd703a44cc362f4783efaac23
panwaver <panwaver@163.com>
2022-01-24 11:28:13 +0800

    删除排序链表中的重复元素优化

--- spring-event/src/main/java/com/pwh/leetcode/linkedList/删除排序链表中的重复元素.java
+++ spring-event/src/main/java/com/pwh/leetcode/linkedList/删除排序链表中的重复元素.java
@@ -8,8 +8,13 @@ import com.pwh.leetcode.util.ListNodeUtils;
  * @date: 2022/1/20 21:23
  */
 public class 删除排序链表中的重复元素 {
+
     class Solution {
+        //双指针技巧
         public ListNode deleteDuplicates(ListNode head) {
+            if (head == null){
+                return head;
+            }
             ListNode slow = head, fast = head;
             while (fast != null) {
                 if (fast.val != slow.val) {
@@ -25,6 +30,22 @@ public class 删除排序链表中的重复元素 {
             slow.next = null;
             return head;
         }
+        //方法二 一次遍历
+        public ListNode deleteDuplicates1(ListNode head) {
+            if (head == null) {
+                return head;
+            }
+            ListNode cur = head;
+            while (cur.next != null) {
+                if (cur.val == cur.next.val) {
+                    cur.next = cur.next.next;
+                } else {
+                    cur = cur.next;
+                }
+            }
+            return head;
+        }
+
     }
 
     public static void main(String[] args) {
