59f1d6f06fa0145e93f823228a3665f5f6b1447a
panwaver <panwaver@163.com>
2022-07-07 19:45:34 +0800

    回环栅栏

--- spring-event/src/main/java/com/pwh/concurrent/CyclicBarrierDemo.java
+++ spring-event/src/main/java/com/pwh/concurrent/CyclicBarrierDemo.java
@@ -11,25 +11,43 @@ import java.util.concurrent.CyclicBarrier;
  * @date 2022/7/6 16:32
  */
 public class CyclicBarrierDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int n = 4;
         CyclicBarrier barrier = new CyclicBarrier(n);
+        CountDownLatch countDownLatch = new CountDownLatch(4);
+//        for (int i = 0; i < n; i++)
+//            new Writer(barrier).start();
         for (int i = 0; i < n; i++)
-            new Writer(barrier).start();
+            new Writer(barrier,countDownLatch,i).start();
+        countDownLatch.await();
     }
 
     static class Writer extends Thread {
         private CyclicBarrier cyclicBarrier;
+        private CountDownLatch countDownLatch;
+        private int i;
 
         public Writer(CyclicBarrier cyclicBarrier) {
             this.cyclicBarrier = cyclicBarrier;
         }
 
+        public Writer(CyclicBarrier cyclicBarrier,CountDownLatch countDownLatch) {
+            this.cyclicBarrier = cyclicBarrier;
+            this.countDownLatch = countDownLatch;
+        }
+
+        public Writer(CyclicBarrier cyclicBarrier,CountDownLatch countDownLatch,int i) {
+            this.cyclicBarrier = cyclicBarrier;
+            this.countDownLatch = countDownLatch;
+            this.i = i;
+        }
+
         @Override
         public void run() {
             try {
-                Thread.sleep(5000); //以睡眠来模拟线程需要预定写入数据操作
+                Thread.sleep(1000 * i); //以睡眠来模拟线程需要预定写入数据操作
                 System.out.println(" 线程" + Thread.currentThread().getName() + " 写入数据完毕，等待其他线程写入完毕");
+                countDownLatch.countDown();
                 cyclicBarrier.await();
             } catch (InterruptedException e) {
                 e.printStackTrace();
